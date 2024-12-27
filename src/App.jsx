45791f54df87f7f7b976a72c7717e3e6f9965690
Imran Ahmad <ahmadimrankh99@gmail.com>
2024-12-27 13:49:37 +0500

    Committed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <section>
+      <div className="header"> 
+        <h1></h1>
+      </div>
+    </section>
+    </>
+    
   )
 }
 
